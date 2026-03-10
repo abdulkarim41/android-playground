caa9459757f8546676c005246cc46e3250e59909
abdulkarim41 <abdulkarim.jatri@gmail.com>
2026-03-10 13:09:57 +0600

    chore(sharedpref): improve SharedPrefHelper implementation and visibility
    - Change visibility of `prefs` property to `private`.
    - Refactor `get` method to remove `inline` and `reified` requirements, switching to type checking based on the `default` parameter.
    - Enhance `IllegalArgumentException` messages in both `put` and `get` methods to include the unsupported type.
    - Add KDoc documentation for the `get` method.

--- library/sharedpref/src/main/kotlin/com/abdulkarim/sharedpref/SharedPrefHelper.kt
+++ library/sharedpref/src/main/kotlin/com/abdulkarim/sharedpref/SharedPrefHelper.kt
@@ -8,9 +8,10 @@ private const val PREF_NAME = "abdulkarim.sharedpref.android.playground"
 
 class SharedPrefHelper(context: Context) {
 
-     val prefs: SharedPreferences by lazy {
+     private val prefs: SharedPreferences by lazy {
         context.applicationContext.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
     }
+
 
     fun put(key: String, value: Any) {
         prefs.edit {
@@ -20,21 +21,26 @@ class SharedPrefHelper(context: Context) {
                 is Int -> putInt(key, value)
                 is Long -> putLong(key, value)
                 is Float -> putFloat(key, value)
-                else -> throw IllegalArgumentException("Unsupported type")
+                else -> throw IllegalArgumentException("Unsupported type: ${value::class}")
             }
         }
     }
 
-    inline fun <reified T> get(key: String, default: T): T {
-        return when (T::class) {
-            String::class -> prefs.getString(key, default as String) as T
-            Boolean::class -> prefs.getBoolean(key, default as Boolean) as T
-            Int::class -> prefs.getInt(key, default as Int) as T
-            Long::class -> prefs.getLong(key, default as Long) as T
-            Float::class -> prefs.getFloat(key, default as Float) as T
-            else -> throw IllegalArgumentException("Unsupported type")
+    /**
+     * Retrieve a value of type String, Boolean, Int, Long, Float
+     */
+    fun <T> get(key: String, default: T): T {
+        @Suppress("UNCHECKED_CAST")
+        return when (default) {
+            is String -> prefs.getString(key, default) as T
+            is Boolean -> prefs.getBoolean(key, default) as T
+            is Int -> prefs.getInt(key, default) as T
+            is Long -> prefs.getLong(key, default) as T
+            is Float -> prefs.getFloat(key, default) as T
+            else -> throw IllegalArgumentException("Unsupported type: ${default?.javaClass?.kotlin}")
         }
     }
+
 
     fun clear() {
         prefs.edit { clear() }
